b01ef2bd99f854f762036cc11f839122c61f27f1
CarlKarlQarl <karlmatthes@gmail.com>
2020-01-17 12:13:15 -0700

    created puppy route

--- main.go
+++ main.go
@@ -2,16 +2,15 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+// structs are objects/classes for Go
+// H below is making a struct
+
 func main() {
 	// capital D means it's exported (public method)
 	// := is the go funky syntx for creating a new variable
 	// but only inside a function
 	// go likes single letter variables
 	router := gin.Default()
-	router.GET("/ping", func(context *gin.Context) {
-		context.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	router.GET("/puppy", handlePuppy)
 	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
